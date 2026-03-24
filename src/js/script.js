84e90ff202f244a7ba24f4d5c85e855a0b43088e
Wellington <welltecnic@hotmail.com>
2026-03-24 11:44:26 -0300

    feat:Declarações e variaveis JS

---
+++ src/js/script.js
@@ -0,0 +1,44 @@
+// Declarações e Variaveis
+//var,let e const
+// var xpto=10;
+// console.log(xpto);
+// var xpto=20;
+// console.log(xpto);
+//var - pode redeclarar e acessar fora do escopo
+//let não pode redeclara e não consegue acessar fora do escopo
+// if(true){
+//     var nome="Fiap";
+// }
+//   console.log(nome);
+
+//const
+// const taxaJuros =0.85;
+// taxaJuros =0.50;
+// console.log(taxaJuros);
+
+var exemplo1="Ola dev var";
+console.log(exemplo1);
+
+let exemplo2="Ola dev let";
+console.log(exemplo2);
+
+const exemplo3="ola dev const";
+console.log(typeof exemplo3);
+
+let exemplo4=20;
+console.log(typeof exemplo4);
+
+let exemplo5=true;
+console.log(typeof exemplo5);
+
+let exemplo6={nome:"fiap"};
+console.log(exemplo6);
+
+let exemplo7=["Java","Python","C#"];
+console.log(typeof exemplo7);
+//variavel indefinida
+let exemplo8;
+console.log(exemplo8);
+//variavel vazia
+let exemplo9=null;
+console.log(exemplo9)
